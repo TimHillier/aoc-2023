653065d98044bdc704442082278ed9070043bc47
Tim Hillier <tim.r.hillier@gmail.com>
2023-12-02 13:30:40 -0800

    Add Template

---
+++ template/day0.py
@@ -0,0 +1,14 @@
+def main():
+    file = open("day0.txt", "r")
+    lines = file.readlines()
+    solution_1(lines)
+    solution_2(lines)
+
+def solution_1(lines):
+    return false
+
+def solution_2(lines):
+    return false
+
+if __name__ == "__main__" :
+    main()
