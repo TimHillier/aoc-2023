ae8a1d16a1420af25aa5a977713dbb246c4dd5cf
Tim Hillier <thillier@acromedia.com>
2023-12-05 17:22:12 -0800

    Day5 solution 2 wip

--- day5/day5.py
+++ day5/day5.py
@@ -12,11 +12,17 @@ def solution_1(lines):
         almanac.append(parse(line))
     alm_arr = get_mapping(almanac)
     seeds = alm_arr.pop(0)[0]
-
     get_location(seeds,alm_arr)
     return False
 
 def solution_2(lines):
+    almanac = []
+    for line in lines:
+        almanac.append(parse(line))
+    alm_arr = get_mapping(almanac)
+    seeds = alm_arr.pop(0)[0]
+    seed_pairs = [[int(seeds[i]), int(seeds[i+1])] for i in range(0, len(seeds) - 1, 2)]
+    get_location_range(seed_pairs, alm_arr)
     return False
 
 def parse(line):
@@ -27,6 +33,36 @@ def parse(line):
     line = line.strip()
     line = line.split(' ')
     return line
+
+def get_location_range(seed_ranges, alm_arr):
+    next = seed_ranges
+########    for section in alm_arr:
+    low = get_next_range(next, alm_arr)
+    print(min(next))
+    return False
+
+def get_next_range(seed_ranges, sections):
+    lowest = -1
+    for array in seed_ranges:
+        seed_start = array[0]
+        seed_stop = array[1]
+        for seed in range(seed_start, (seed_start + seed_stop)):
+            #            print("Testing Seed: " + str(seed))
+            next = [seed]
+            for section in sections:
+                next = get_next(next, section)
+
+#            print("Location: " + str(next))
+            if (lowest == -1):
+                lowest = next 
+                continue
+            if (lowest > next):
+                lowest = next 
+                continue
+
+    print("Lowest: " + str(lowest))
+    return False
+
 
 def get_mapping(almanac):
     alm_arr = []
@@ -45,41 +81,25 @@ def get_mapping(almanac):
 def get_location(seeds, alm_arr):
     next = seeds
     for section in alm_arr:
-        next = get_next(next, section) # I think this is the part that loops.
-        print(next)
+        next = get_next(next, section)
     print(min(next))
     return False
 
 def get_next(seeds, section):
     next_seeds = []
+    seeds = list(map(int,seeds))
     for array in section:
         source_start = int(array[1])
         source_end = source_start + int(array[2]) - 1
         destination_start = int(array[0])
-        destination_end = destination_start+ int(array[2]) - 1
-
         found_seeds = []
         for seed in seeds:
-            seed = int(seed)
             if(seed >= source_start and seed <= source_end):
-                print(seed - source_start)
                 seed_index = seed - source_start
                 next_seeds.append(destination_start + seed_index)
                 found_seeds.append(seed)
-
-        diff_seeds = list((set(list(map(int, seeds))) - set(found_seeds)))
-        return next_seeds + diff_seeds
-
-
-
-#        rejected_seeds = []
-#       found_seeds = []
-#       for seed in seeds:
-#           seed = int(seed)
-#           if(seed in source_array):
-#               found_seeds.append(seed)
-#               next_seeds.append(destination_array[source_array.index(seed)])
-
+        seeds = [x for x in seeds if x not in found_seeds]
+    return next_seeds + seeds
 
 if __name__ == "__main__" :
     main()
